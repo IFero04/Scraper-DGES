d071d1e39eed91c0f81377363a2a72f58c9bf646
Afonso Faria <afonsofariatech@gmail.com>
2024-04-19 00:07:23 +0100

    Fix , problem writing to csv

--- csv_save.py
+++ csv_save.py
@@ -9,6 +9,7 @@ class Item:
    
 def export_csv(data, filename):
     with open(f'csv/{filename}', "w", encoding="utf-8") as file:
-        file.write("Código Curso,Curso,Nº Identificação(parcial),Nome\n")
+        file.write("Código Curso;Curso;Nº Identificação(parcial);Nome\n")
         for item in data:
-            file.write(f"{item.cod_curso},{item.curso},{item.id},{item.nome}\n")
+            file.write(f"{item.cod_curso};{item.curso};{item.id};{item.nome}\n")
+
